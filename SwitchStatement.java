5690b2043645667dc74b364fddf998577b758e09
greed-d <deekshit.bti@gmail.com>
2024-10-19 12:10:38 +0545

    `add` : switch statement stuff

---
+++ SwitchStatement.java
@@ -0,0 +1,27 @@
+public class SwitchStatement {
+  public static void main(String[] args) {
+    int n = 6;
+    switch(n){
+      case 1:
+        System.out.println("Sunday");
+        break;
+      case 2:
+        System.out.println("Monday");
+        break;
+      case 3:
+        System.out.println("Tuesday");
+        break;
+      case 4:
+        System.out.println("Wednesday");
+        break;
+      case 5:
+        System.out.println("Thrusday");
+        break;
+      case 6:
+        System.out.println("Friday");
+        break;
+      default:
+        System.out.println("Enter a valid number");
+    }
+  }
+}
